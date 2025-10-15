44567a17f8b2c82cba5de1f98df91bf30681d9df
natthamon0 <natthamon.k@merudy.com>
2025-10-15 12:35:36 +0700

    v3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'little-inn/app',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '84c1237250b60a21284dff54714795a97a3aa7d7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'little-inn/app' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '84c1237250b60a21284dff54714795a97a3aa7d7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
